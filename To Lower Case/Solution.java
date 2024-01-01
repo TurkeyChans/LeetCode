e63b47611e99e83ccfdb0f4e3de321eff53f5a8b
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2023-12-31 20:52:39 -0500

    Add files via upload

---
+++ To Lower Case/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        Solution test = new Solution();
+        String s = "TESTING";
+        System.out.println("Before: " + s);
+        System.out.println("After: " + test.toLowerCase(s));
+    }
+    
+        
+        
+
+}
